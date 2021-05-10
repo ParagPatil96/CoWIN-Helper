ad192cba02c49d57204a5df4c8adde46dd8606bc
ParagPatil96 <patilparag96@gmail.com>
2021-05-10 13:59:01 +0530

    init

---
+++ app/src/main/java/com/patilparag96/cowinhelper/MainActivity.java
@@ -0,0 +1,14 @@
+package com.patilparag96.cowinhelper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
